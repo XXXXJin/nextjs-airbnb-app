d74ed375bd03283bb1d5e3a437645c22765a9d0e
XXXXJin <jinhaixu@hotmail.co.jp>
2024-03-19 22:50:09 +0900

    検索ステートをurlに保存する処理を追加

--- app/ui/dashboard/Search.tsx
+++ app/ui/dashboard/Search.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("q", e.target.value);
+    replace(`${pathName}?${params.toString()}`);
+  };
+
   return (
     <div className="flex items-center gap-3 bg-gray-800 p-2 rounded-md">
       <MdSearch />
@@ -9,6 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         type="text"
         placeholder={placeholder}
         className="bg-transparent border-none focus:outline-none"
+        onChange={handleSearch}
       />
     </div>
   );
