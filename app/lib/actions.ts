1bf2fa43204c93aa799f2185c337a4964c4109d5
XXXXJin <jinhaixu@hotmail.co.jp>
2024-04-16 15:36:18 +0900

    サーバーアクション関数を追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,16 +1,21 @@
 "use server";
-import { User } from "./models";
+import { revalidatePath } from "next/cache";
+import { Product, User } from "./models";
 import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
 
 export const addUser = async (formData: FormData) => {
   const { username, email, password, phone, address, isAdmin, isActive } =
     Object.fromEntries(formData);
   try {
     connectToDB();
+    const salt = await bcrypt.genSalt(10);
+    const hashdedPassword = bcrypt.hash(password as string, salt);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashdedPassword,
       phone,
       address,
       isAdmin,
@@ -22,4 +27,31 @@ export const addUser = async (formData: FormData) => {
     console.log(error);
     throw new Error("failed to add user");
   }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
+
+export const addProduct = async (formData: FormData) => {
+  const { title, desc, price, stock, color, size } =
+    Object.fromEntries(formData);
+  try {
+    connectToDB();
+    const newProduct = new Product({
+      title,
+      desc,
+      price,
+      stock,
+      color,
+      size,
+    });
+
+    await newProduct.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to add products");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
 };
